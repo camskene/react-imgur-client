53adead50939088a8d195075d95a51aa375c026e
Cam Skene <camskene@gmail.com>
2015-09-20 17:04:57 -0400

    Imgur Client
    Imgur Client with React, Reflux, and React Router.

---
+++ README.md
@@ -0,0 +1,2 @@
+### IMGUR Client
+
---
+++ src/stores/topic-store.jsx
@@ -0,0 +1,21 @@
+var Api = require('../utils/api');
+var Reflux = require('reflux');
+var Actions = require('../actions');
+
+var TopicStore = Reflux.createStore({
+  listenables: [Actions],
+  getTopics: function() {
+    Api.get('topics/defaults')
+      .then(function(json) {
+        this.topics = json.data;
+        this.triggerChange();
+      }.bind(this));
+  },
+  triggerChange: function() {
+    this.trigger('change', this.topics);
+  }
+});
+
+
+
+module.exports = TopicStore;
